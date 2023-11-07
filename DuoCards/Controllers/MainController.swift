5b00963a6f7300433c294497d239d142ebc4b505
Pavel Goldman <baal.private@gmail.com>
2023-11-07 12:55:53 +0200

    Deploying the Application Foundation

---
+++ DuoCards/Controllers/MainController.swift
@@ -0,0 +1,15 @@
+//
+//  MainController.swift
+//  DuoCards
+//
+//  Created by Pavel Goldman on 07.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class MainController {
+    
+    let model:DuoCardsModel = DuoCardsModel()
+    
+}
---
+++ DuoCards/Views/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuoCards
+//
+//  Created by Pavel Goldman on 07.11.2023.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
