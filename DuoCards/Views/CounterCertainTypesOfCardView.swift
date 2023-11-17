bf0d4413e7a817eca9fc32c64c10cf796d81301c
Pavel Goldman <baal.private@gmail.com>
2023-11-17 16:53:44 +0200

    Developing1

---
+++ DuoCards/Views/CounterCertainTypesOfCardView.swift
@@ -0,0 +1,86 @@
+//
+//  CounterCertainTypesOfCardController.swift
+//  DuoCards
+//
+//  Created by Pavel Goldman on 17.11.2023.
+//
+
+import UIKit
+
+class CounterCertainTypesOfCardView: UIView {
+    
+    var typeCard: LearningPhase?
+    var countCards: Int?
+    let widthView = (UIScreen.main.bounds.width - 80) / 3
+    
+    //we use lazy properties for each view
+    
+    lazy var headerTitle: UILabel = {
+        let headerTitle = UILabel(frame: CGRect(x: 0, y: 55, width: widthView, height: 20))
+        headerTitle.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        headerTitle.textAlignment = .center
+        return headerTitle
+    }()
+    
+    lazy var countTitle: UILabel = {
+        let countTitle = UILabel(frame: CGRect(x: 0, y: 15, width: widthView, height: 30))
+        if let count = countCards { countTitle.text = String(count) }
+        countTitle.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        countTitle.textAlignment = .center
+        return countTitle
+    }()
+    
+    lazy var headerView: UIView = {
+        print(widthView)
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: widthView, height: widthView - 20))
+        headerView.backgroundColor = .white
+        headerView.layer.borderWidth = 0.5
+        headerView.layer.borderColor = UIColor.gray.cgColor
+        headerView.layer.cornerRadius = 5
+        headerView.addSubview(headerTitle)
+        headerView.addSubview(countTitle)
+        return headerView
+    }()
+    
+    init(frame:CGRect,typeCard: LearningPhase, countCards: Int) {
+        self.typeCard = typeCard
+        self.countCards = countCards
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    
+    func setupView() {
+        
+        let imageQuestionMark = UIImage(systemName: "questionmark.circle")
+        
+        if let type = typeCard {
+            switch type {
+            case .toLearn:
+                countTitle.textColor = UIColor(named: "GreenColor")
+                headerTitle.textColor = UIColor(named: "GreenColor")
+                headerTitle.addTrailing(image: imageQuestionMark, text: type.rawValue, color: UIColor(named: "GreenColor") ?? .green)
+                
+            case .notCompleted:
+                countTitle.textColor = .systemBlue
+                headerTitle.textColor = .systemBlue
+                headerTitle.addTrailing(image: imageQuestionMark, text: type.rawValue, color: .systemBlue)
+            case .completed:
+                countTitle.textColor = .systemYellow
+                headerTitle.textColor = .systemYellow
+                headerTitle.addTrailing(image: imageQuestionMark, text: type.rawValue, color: .systemYellow)
+            case nil:
+                countTitle.textColor = .green
+                headerTitle.textColor = .green
+            }
+        }
+        
+        self.addSubview(headerView)
+    }
+    
+}
---
+++ DuoCards/Views/MainViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  DuoCards
+//
+//  Created by Pavel Goldman on 07.11.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    lazy var startButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("START", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
+    let stackView = UIStackView()
+    let widthView = (UIScreen.main.bounds.width - 80) / 3
+    let backgroundImage = UIImage(named: "backgroundMain")
+    let backgroundView = UIImageView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAllViews()
+        self.view.backgroundColor = UIColor(named: "BackGroundAccentColor")
+    }
+    
+    
+    func loadAllViews() {
+        configureStackView()
+        addConstraintToBackGroundImage()
+        addConstraintToStartButton()
+    }
+    
+    func configureStackView() {
+        view.addSubview(stackView)
+        addViewToStackView()
+        setStackViewConstraints()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+    }
+    
+    func setStackViewConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints                                                          = false
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive           = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive   = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: widthView - 20).isActive                                              = true
+    }
+    
+    
+    func addViewToStackView() {
+        let typesCards:[LearningPhase] = [LearningPhase.toLearn,LearningPhase.notCompleted,LearningPhase.completed]
+        
+        for typeCard in typesCards {
+            let frame = CGRect(x: 0, y: 0, width: widthView, height: widthView)
+            let countCards = MainController.shared.getCountOfCardsCertainPhase(phase: typeCard)
+            let viewCard = CounterCertainTypesOfCardView(frame: frame, typeCard: typeCard, countCards: countCards)
+            stackView.addArrangedSubview(viewCard)
+        }
+    }
+    //MARK: Constraint to Background Image
+    private func addConstraintToBackGroundImage() {
+        view.addSubview(backgroundView)
+        if let img = backgroundImage {
+            backgroundView.image = img
+            backgroundView.translatesAutoresizingMaskIntoConstraints = false
+            backgroundView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 100).isActive = true
+            backgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 20).isActive = true
+            backgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+            backgroundView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        }
+    }
+    
+    //MARK: Constraint to Start Button
+    private func addConstraintToStartButton() {
+        view.addSubview(startButton)
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        startButton.topAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: 100).isActive = true
+        startButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 60).isActive = true
+        startButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -60).isActive = true
+        startButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    
+    
+    
+}
+
